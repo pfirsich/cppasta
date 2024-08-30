ea8c9a1df9a99afbce0a9bcac7a7e5ed3f275a00
Joel Schumacher <joelschum@gmail.com>
2024-08-30 12:47:18 +0200

    Add typed integer literal suffixes

--- include/cppasta/primitive_typedefs.hpp
+++ include/cppasta/primitive_typedefs.hpp
@@ -17,3 +17,53 @@ using isize = i64;
 
 using f32 = float;
 using f64 = double;
+
+consteval u8 operator""_u8(unsigned long long int v)
+{
+    return static_cast<u8>(v);
+}
+
+consteval u16 operator""_u16(unsigned long long int v)
+{
+    return static_cast<u16>(v);
+}
+
+consteval u32 operator""_u32(unsigned long long int v)
+{
+    return static_cast<u32>(v);
+}
+
+consteval u64 operator""_u64(unsigned long long int v)
+{
+    return static_cast<u64>(v);
+}
+
+consteval i8 operator""_i8(unsigned long long int v)
+{
+    return static_cast<i8>(v);
+}
+
+consteval i16 operator""_i16(unsigned long long int v)
+{
+    return static_cast<i16>(v);
+}
+
+consteval i32 operator""_i32(unsigned long long int v)
+{
+    return static_cast<i32>(v);
+}
+
+consteval i64 operator""_i64(unsigned long long int v)
+{
+    return static_cast<i64>(v);
+}
+
+consteval usize operator""_usize(unsigned long long int v)
+{
+    return static_cast<usize>(v);
+}
+
+consteval isize operator""_isize(unsigned long long int v)
+{
+    return static_cast<isize>(v);
+}
